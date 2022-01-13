a3b63b75f5e7cf561a44b1b6bc98c262193a649a
andrewprok <andrewprok@gmail.com>
2022-01-13 15:10:19 +0300

    feat: Setup i18n provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { i18n } from '@lingui/core';
+import { I18nProvider } from '@lingui/react';
 import Inbox from './inbox';
+import { messages } from './locales/en/messages';
+
+i18n.load('en', messages);
+i18n.activate('en');
 
 function App() {
-  return <Inbox />;
+  return (
+    <I18nProvider i18n={i18n}>
+      <Inbox />
+    </I18nProvider>
+  );
 }
 
 export default App;
